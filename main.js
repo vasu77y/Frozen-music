afa1571aad349a2e30ce9f12da920956f04ae376
vasu77y <78721279+vasu77y@users.noreply.github.com>
2025-04-15 17:48:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 });
 
 // Add your bot token here.
-const BOT_TOKEN = "7967527697:AAF4x6l--sTTh5gQ9_mWpNa64oTILohLo4k";
+const BOT_TOKEN = "7688408787:AAH4YEXTtC_P7OcDwPkFMVmtUEwkOkfscU0";
 
 //Pro people: don't change this else your bot will be fucked.
 const API_BASE_URL = "https://probable-berti-frozenbotspvt-17e82b7b.koyeb.app";
